44a50d2e73533f597e0dddf21489b24030e1f798
skyrimax <maxroyal123@hotmail.com>
2021-01-20 22:46:03 -0500

    Ajouter le code de la partie 3

---
+++ pi/Lab1/Partie 3/Thread_Cligne_10_Hz_time.c
@@ -0,0 +1,100 @@
+/*
+ * Thread_Cligne_10_Hz_time .c
+ *
+ * Ce programme fait clignoter un DEL à une fréquence de 10 Hz .
+ * Le clignotement se fait à l’intérieur d’un thread et le délai est
+ * ajusté avec des fonctions de la librarie " time .h".
+ * L’arrêt sera provoqué par un appui sur le bouton-poussoir 1.
+ * Aucun paramètre en entrée.
+ *
+ */
+
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <bcm2835.h>
+ #include <pthread.h>
+ #include <time.h>
+
+ double frequence = 10.0; // Hertz : fréquence de clignotement désirée
+
+ /* Fonction clignote qui sera appelée par le thread " cligne "
+ *
+ * Elle fait clignoter le DEL rouge à 1 Hertz .
+ *
+ * La fonction ne retourne aucune valeur .
+ * La fonction n’exige aucun param ètre en entrée.
+ */
+ void *clignote()
+ {
+    clock_t debut, fin ; // Variables de temps
+    double demiPer = 1/(2.0*frequence); // Calcul de la demi-période
+    int etat = 0; // État du DEL
+    
+    debut = clock(); // Temps écoulé depuis le lancement du programme
+
+    // Boucle infinie pour le clignotement du DEL
+    while (1) {
+      if (etat==0) {
+        bcm2835_gpio_write(20 , HIGH );
+        etat = 1;
+      }
+      else {
+        bcm2835_gpio_write(20 , LOW );
+        etat = 0;
+      }
+
+      fin = clock(); // Temps écoulé depuis le lancement du programme
+
+      // La différence (fin - debut) donne le temps d’exécution du code
+      // On cherche une durée égale à demiPer. On compense avec un délai.
+      // CLOCK_PER_SEC est le nombre de coups d’horloges en 1 seconde
+
+      usleep(1000000*(demiPer -((double) (fin - debut)/((double) CLOCKS_PER_SEC))));
+      debut = fin;
+    }
+    pthread_exit( NULL );
+ }
+
+ /* Fonction main
+  *
+  * Elle configure le GPIO et le thread.
+  *
+  * La fonction ne retourne aucune valeur.
+  * La fonction n’exige aucun paramètre en entrée.
+  */
+ int main( int argc , char ** argv )
+ {
+    // Identificateur du thread
+    pthread_t cligne ;
+
+    // Initialisation du bcm2835
+    if (!bcm2835_init()){
+        return 1;
+    }
+
+    // Configuration du GPIO pour DEL 1 ( rouge )
+    bcm2835_gpio_fsel(20 , BCM2835_GPIO_FSEL_OUTP );
+    // Configuration du GPIO pour bouton - poussoir 1
+    bcm2835_gpio_fsel(19 , BCM2835_GPIO_FSEL_INPT );
+
+    // Création du thread "cligne".
+    // Lien avec la fonction clignote.
+    // Cette dernière n’exige pas de paramètres.
+    pthread_create(&cligne, NULL, &clignote, NULL );
+
+    // Boucle tant que le bouton - poussoir est non enfoncé
+    while ( bcm2835_gpio_lev(19) ){
+      usleep(1000) ; // Délai de 1 ms !!!
+    }
+
+    // Si bouton - poussoir enfoncé, arrêt immédiat du thread
+    pthread_cancel(cligne);
+    // Attente de l’arrêt du thread
+    pthread_join(cligne , NULL );
+
+    // Éteindre le DEL rouge
+    bcm2835_gpio_write(20 , LOW );
+    // Libérer le GPIO
+    bcm2835_close();
+    return 0;
+ }
